7047fd1d7280f2d84398fcd26596e56d672083a9
John Barbero Unenge <git@lsrkttn.com>
2023-08-06 14:10:58 +0200

    [keymap] Add my own corne layout

---
+++ keyboards/crkbd/keymaps/JBarberU/rebuild.sh
@@ -0,0 +1,2 @@
+qmk json2c $HOME/dotfiles/kbdlayout/corne/jbarberus-corne.json > keymap.c
+qmk flash -kb crkbd/r2g -km JBarberU
---
+++ keyboards/crkbd/keymaps/JBarberU/rules.mk
@@ -0,0 +1,19 @@
+# Enable features
+OLED_ENABLE  = yes
+EXTRAKEY_ENABLE = yes # for media keys
+MOUSEKEY_ENABLE = yes
+BOOTLOADER = caterina
+
+# Size optimisations
+AUDIO_ENABLE = no           # Audio output on port C6
+AUTO_BUTTONS = no
+COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug
+GRAVE_ESCAPE_ENABLE = no
+LTO_ENABLE = yes
+MIDI_ENABLE = no            # MIDI controls
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+SPACE_CADET_ENABLE = no
+TAP_DANCE_ENABLE = no
+WPM_ENABLE = no
