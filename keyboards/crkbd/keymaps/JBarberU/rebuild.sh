7acfd5cb855f00b0671b5e7c3dd4c24d3b806c46
John Barbero Unenge <git@lsrkttn.com>
2023-08-11 23:58:44 +0200

    [keymap] Tweak rebuild script

--- keyboards/crkbd/keymaps/JBarberU/rebuild.sh
+++ keyboards/crkbd/keymaps/JBarberU/rebuild.sh
@@ -1,2 +1,15 @@
-qmk json2c jbarberus-corne.json > keymap.c
+#!/bin/bash
+
+REGULAR="jbarberus-corne.json"
+
+if [ -z "$1" ]; then
+    CONFIG_FILE="$REGULAR"
+    echo "No config file given, using default"
+else
+    CONFIG_FILE="$1"
+fi
+
+echo "CONFIG_FILE=$CONFIG_FILE"
+
+qmk json2c "$CONFIG_FILE" > keymap.c
 qmk flash -kb crkbd/r2g -km JBarberU
