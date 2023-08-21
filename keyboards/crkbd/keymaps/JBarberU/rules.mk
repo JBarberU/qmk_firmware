e856dc22abc4d063f123e432802dde1358ca6955
John Barbero Unenge <git@lsrkttn.com>
2023-08-21 21:47:40 +0200

    [keymap] Disable mouse keys
    They were causing problems at bootup and forcing the keyboard to be
    unplugged and replugged with each startup.

--- keyboards/crkbd/keymaps/JBarberU/rules.mk
+++ keyboards/crkbd/keymaps/JBarberU/rules.mk
@@ -1,17 +1,24 @@
+# General configurations
+BOOTLOADER = caterina
+
 # Enable features
 OLED_ENABLE  = yes
 EXTRAKEY_ENABLE = yes # for media keys
-MOUSEKEY_ENABLE = yes
-BOOTLOADER = caterina
+NKRO_ENABLE = yes
+
+# Disable features
+# Disabled to prevent having to unplug and replug keyboard
+# at every boot of Dell laptops
+MOUSEKEY_ENABLE = no
 
 # Size optimisations
+LTO_ENABLE = yes
 AUDIO_ENABLE = no           # Audio output on port C6
 AUTO_BUTTONS = no
 BOOTMAGIC_ENABLE = no
 COMMAND_ENABLE = no         # Commands for debug and configuration
 CONSOLE_ENABLE = no         # Console for debug
 GRAVE_ESCAPE_ENABLE = no
-LTO_ENABLE = yes
 MIDI_ENABLE = no            # MIDI controls
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = no
