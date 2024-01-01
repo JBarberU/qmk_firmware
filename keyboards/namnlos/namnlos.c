1419db357d93b70470632969e4c1eea5f96240d1
John Barbero Unenge <git@lsrkttn.com>
2024-01-01 22:54:34 +0100

    [keyboard] Tweak oled code

--- keyboards/namnlos/namnlos.c
+++ keyboards/namnlos/namnlos.c
@@ -5,65 +5,60 @@
 
 // oled keylog rendering has been kindly borrowed from crkbd <3
 
-char     key_name = ' ';
-uint16_t last_keycode;
 uint8_t  last_row;
 uint8_t  last_col;
-
-static const char PROGMEM code_to_name[60] = {' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\', '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
 
 static void set_keylog(uint16_t keycode, keyrecord_t *record) {
     last_row = record->event.key.row;
     last_col = record->event.key.col;
-
-    key_name     = ' ';
-    last_keycode = keycode;
-    if (IS_QK_MOD_TAP(keycode)) {
-        if (record->tap.count) {
-          keycode = QK_MOD_TAP_GET_TAP_KEYCODE(keycode);
-        } else {
-          keycode = 0xE0 + biton(QK_MOD_TAP_GET_MODS(keycode) & 0xF) + biton(QK_MOD_TAP_GET_MODS(keycode) & 0x10);
-        }
-    } else if (IS_QK_LAYER_TAP(keycode) && record->tap.count) {
-        keycode = QK_LAYER_TAP_GET_TAP_KEYCODE(keycode);
-    } else if (IS_QK_MODS(keycode)) {
-        keycode = QK_MODS_GET_BASIC_KEYCODE(keycode);
-    } else if (IS_QK_ONE_SHOT_MOD(keycode)) {
-        keycode = 0xE0 + biton(QK_ONE_SHOT_MOD_GET_MODS(keycode) & 0xF) + biton(QK_ONE_SHOT_MOD_GET_MODS(keycode) & 0x10);
-    }
-    if (keycode > ARRAY_SIZE(code_to_name)) {
-        return;
-    }
-
-    // update keylog
-    key_name = pgm_read_byte(&code_to_name[keycode]);
-}
-
-static const char *depad_str(const char *depad_str, char depad_char) {
-    while (*depad_str == depad_char) {
-        ++depad_str;
-    }
-    return depad_str;
 }
 
 static void oled_render_keylog(void) {
     oled_write_char('0' + last_row, false);
     oled_write("x", false);
     oled_write_char('0' + last_col, false);
-    oled_write(", k", false);
-    const char *last_keycode_str = get_u16_str(last_keycode, ' ');
-    oled_write(depad_str(last_keycode_str, ' '), false);
-    oled_write(":", false);
-    oled_write_char(key_name, false);
 }
 
 __attribute__((weak)) const char * get_layer_name_user(uint8_t layer) {
     return get_u8_str(layer, ' ');
 }
 
+oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;
+    }
+    else {
+        return OLED_ROTATION_270;
+    }
+    return rotation;
+}
+
+__attribute__((weak)) void oled_render_logo_user(void) {
+    static const char qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write(qmk_logo, false);
+}
+
 static void oled_render_layer(void) {
-    oled_write("Layer: ", false);
-    oled_write_ln(get_layer_name_user(get_highest_layer(layer_state)), false);
+    oled_write_ln("Layer", false);
+    const char* layer_name = get_layer_name_user(get_highest_layer(layer_state));
+    if (strlen(layer_name) % 5 == 0) {
+        oled_write(layer_name, false);
+    }
+    else {
+        oled_write_ln(layer_name, false);
+    }
+}
+
+void oled_newlines(int num_lines) {
+    for (unsigned int i = 0; i < num_lines; ++i)
+    {
+        oled_advance_page(true);
+    }
 }
 
 bool oled_task_kb(void) {
@@ -71,18 +66,37 @@ bool oled_task_kb(void) {
         return false;
     }
 
-    oled_render_layer();
-    oled_render_keylog();
-    oled_advance_page(true);
+    if (is_keyboard_master())
+    {
+        oled_render_keylog();
+        oled_newlines(5);
+        oled_render_layer();
+    }
+    else
+    {
+        oled_render_logo_user();
+    }
     return false;
 }
 
 static void setupForFlashing(void) {
     oled_clear();
-    oled_write("                     ", false);
-    oled_write("  In flash mode...   ", false);
-    oled_write("                     ", false);
-    oled_write("                     ", false);
+    oled_write("     ", false);
+    oled_write("     ", false);
+    oled_write("     ", false);
+    oled_write("*****", false);
+    oled_write("     ", false);
+    oled_write("Flash", false);
+    oled_write("     ", false);
+    oled_write("Mode ", false);
+    oled_write("     ", false);
+    oled_write("*****", false);
+    oled_write("     ", false);
+    oled_write("     ", false);
+    oled_write("     ", false);
+    oled_write("     ", false);
+    oled_write("     ", false);
+    oled_write("     ", false);
 
     // Force data to be rendered
     oled_render_dirty(true);
