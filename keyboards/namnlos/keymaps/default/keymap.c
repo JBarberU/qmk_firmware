ea1c2a211184b6bbdf891b8bfab7d9e85f297696
John Barbero Unenge <git@lsrkttn.com>
2025-03-31 21:18:57 +0200

    [keyboard] Add gaming layer

--- keyboards/namnlos/keymaps/default/keymap.c
+++ keyboards/namnlos/keymaps/default/keymap.c
@@ -7,6 +7,7 @@ enum {
     L_Base,
     L_Lower,
     L_Raise,
+    L_Gaming
 };
 
 #define MO_Ra LT(L_Raise, KC_BSPC)
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,            KC_NO,   KC_DEL,   MO_Ra, KC_LSFT,      KC_RSFT, MO_Lo,   KC_ENT,  KC_RALT,          KC_NO,   KC_NO
     ),
     [L_Raise] = LAYOUT(
-        QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TO(L_Gaming),
         KC_NO,   KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_EXLM,                      KC_CIRC, KC_AMPR, KC_MINS, KC_EQL,  KC_RBRC, KC_TILD,
         KC_NO,   KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, KC_DLR,                       KC_BSLS, KC_UNDS, KC_LPRN, KC_RPRN, KC_PLUS, KC_GRV,
         KC_NO,   KC_NO,   KC_NO,   KC_PERC, KC_HASH,  KC_AT,                        KC_ASTR, KC_LBRC, KC_LCBR, KC_RCBR, KC_PIPE, KC_NO,
@@ -51,7 +52,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,                         KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
         KC_NO,   KC_NO,            KC_NO,   KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_NO,    KC_NO
-
+    ),
+    [L_Gaming] = LAYOUT(
+        KC_NO,   KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
+        KC_TAB,  KC_NO,  KC_NO,   KC_COMM, KC_D,     KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_ESC,  KC_NO,  KC_A,    KC_SCLN, KC_H,     KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_I,     KC_V,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,           KC_NO,   KC_NO,    KC_NO,   KC_NO,        KC_NO, TO(L_Base),   KC_NO,   KC_NO,            KC_NO,   KC_NO
     )
     /* Blank layer
     [<< layer name >>] = LAYOUT(
@@ -72,6 +79,8 @@ const char * get_layer_name_user(uint8_t layer) {
             return "Lower";
         case L_Raise:
             return "Raise";
+        case L_Gaming:
+            return "Gamin";
         default:
             return "Undef";
     }
