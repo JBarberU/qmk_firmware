ff59a520c9ef5a9030de5741bc4793105cee0e19
John Barbero Unenge <git@lsrkttn.com>
2025-07-01 22:56:22 +0200

    Prep for real board

--- keyboards/jbarberu/heimdall/display.c
+++ keyboards/jbarberu/heimdall/display.c
@@ -87,7 +87,9 @@ bool display_init_kb(void) {
 
     backlight_enable();
 
-    painter_device_t display = qp_st7789_make_spi_device(240, 300, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 16, 3);
+    const uint16_t SPI_MODE = 3;
+    const uint16_t SPI_DIVISOR = 16;
+    painter_device_t display = qp_st7789_make_spi_device(240, 300, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, SPI_DIVISOR, SPI_MODE);
     qp_set_viewport_offsets(display, 0, 20);
 
     if (!qp_init(display, QP_ROTATION_180) || !qp_power(display, true) || !qp_lvgl_attach(display)) return false;
