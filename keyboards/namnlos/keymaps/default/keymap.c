152b20192beaaa64e4681c114a16052fd0120011
John Barbero Unenge <git@lsrkttn.com>
2026-03-02 20:32:55 +0100

    Add bindings to gaming layer

--- keyboards/namnlos/keymaps/default/keymap.c
+++ keyboards/namnlos/keymaps/default/keymap.c
@@ -56,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,            KC_NO,   KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_NO,    KC_NO
     ),
     [L_Gaming] = LAYOUT(
-        KC_NO,   KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
-        KC_TAB,  KC_NO,  KC_NO,   KC_COMM, KC_D,     KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_ESC,  KC_NO,  KC_A,    KC_SCLN, KC_H,     KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_I,     KC_V,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,           KC_NO,   KC_NO,    KC_NO,   KC_NO,        KC_NO, TO(L_Base),   KC_NO,   KC_NO,            KC_NO,   KC_NO
+        KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  TO(L_Base),
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LALT,          KC_M,    KC_N,     KC_LCTL,   KC_Y,      KC_RSFT, MO_Lo,   KC_ENT,  KC_RALT,          KC_NO,   KC_NO
     )
     /* Blank layer
     [<< layer name >>] = LAYOUT(
